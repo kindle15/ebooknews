c40bd50038176b43e336362ab144afb3bb82cfbd
rbr <rbr@arcor.de>
2020-01-19 17:28:12 +0100

    CHORE(basic): add service docker files

---
+++ docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DEBUG=true
+
+while true; do
+    tail -f /dev/null
+done
+
+exit # This may never run. Why is it here then? It's pretty.
+     # Kind of like, "The End" at the end of a movie that you
+     # already know is over. It's always nice.
